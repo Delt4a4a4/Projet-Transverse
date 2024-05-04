14acd932e7682222aa36d83c0bb70b733d1f032d
MoanaWong <moanawong2@gmail.com>
2024-05-04 12:11:22 +0200

    ?

--- Panier.py
+++ Panier.py
@@ -5,8 +5,8 @@ class Panier(pygame.sprite.Sprite):
     def __init__(self,Game):
         super().__init__()
         # Paramètres du ballon
-        self.image = pygame.image.load("Image/Ballon.png")
-        self.image = pygame.transform.scale(self.image, (100, 100))  # taille de l'image du ballon à remplir
+        self.image = pygame.image.load("Image/Panier.png")
+        self.image = pygame.transform.scale(self.image, (100, 85))  # taille de l'image du ballon à remplir
         self.rect = self.image.get_rect()
         self.rect.x = random.randint(400, 800)
         self.rect.y = random.randint(1, 500)
